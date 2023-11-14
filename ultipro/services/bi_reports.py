81a50fe85e0f2bbb82120daada1f68ed82300e08
Javier <javier2mil8@gmail.com>
2023-11-14 17:22:57 -0300

    back to decoding

--- ultipro/services/bi_reports.py
+++ ultipro/services/bi_reports.py
@@ -4,4 +4,4 @@ def execute_and_fetch(client, report_path, delimiter=','):
     context = bi_data.log_on_with_token(client)
     k = bi_data.execute_report(client, context, report_path, delimiter=delimiter)
     r = bi_stream.retrieve_report(client, k)
-    return r['body']['ReportStream']
+    return r['body']['ReportStream'].decode('utf-8')
