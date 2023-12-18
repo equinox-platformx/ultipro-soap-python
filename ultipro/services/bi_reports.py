e68fbeb45ede56e2b768abacdc9c79ad542861e8
Javier <javier2mil8@gmail.com>
2023-12-18 18:43:49 -0300

    added debug prints

--- ultipro/services/bi_reports.py
+++ ultipro/services/bi_reports.py
@@ -6,7 +6,7 @@ from ultipro.services import bi_data, bi_stream
 def execute_and_fetch(client, report_path, delimiter=','):
     context = bi_data.log_on_with_token(client)
     pprint(context)
-    pprint("report path: " + report_path)
+    print("report path: " + report_path)
     k = bi_data.execute_report(client, context, report_path, delimiter=delimiter)
     pprint(k)
     r = bi_stream.retrieve_report(client, k)
